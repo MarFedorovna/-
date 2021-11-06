224c93e261b344d400e420eb4a66aa574935755c
kolyvanmarin <91743581+kolyvanmarin@users.noreply.github.com>
2021-11-06 10:23:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def pi(n):
     while k < n:
         ak = -Decimal((6 * k - 5) * (2 * k - 1) * (6 * k - 1)) / Decimal(k * k * k * 26680 * 640320 * 640320)
         val = ak * (13591409 + 545140134 * k)
-        d = Decimal((6 * k - 5) * (2 * k - 1) * (6 * k - 1)) / Decimal(k * k * k * 26680 * 640320 * 640320)
+        decim = Decimal((6 * k - 5) * (2 * k - 1) * (6 * k - 1)) / Decimal(k * k * k * 26680 * 640320 * 640320)
         pi += val
         k += 1
     pi = pi * Decimal(10005).sqrt() / 4270934400
